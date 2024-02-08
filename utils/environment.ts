0bcb78acca226c4c1ce0ae6779b9c3073eacaa89
Kaled Barreto <kaledbarreto@gmail.com>
2024-02-08 11:56:57 -0300

    feat: add missing env file

--- utils/environment.ts
+++ utils/environment.ts
@@ -1,1 +1,28 @@
-//API URL
+export type Environment = 'development' | 'production';
+
+const  environments: Record<string, Environment> = {
+    localhost: 'development',
+    'develop.url.dev': 'development',
+    'production.url': 'production',
+}
+
+const apiUrls: Record<Environment, string> = {
+    development: 'https://college-hack-api.azurewebsites.net',
+    production: 'production.url',
+}
+
+export function environment(env: Environment): boolean {
+    const { hostname } = window.location;
+
+    return environments[hostname] === env;
+}
+
+export function getEnvironment(): Environment {
+    const { hostname } = window.location;
+
+    return environments[hostname];
+}
+
+export function getApiURL() {
+    return apiUrls[getEnvironment()];
+}
