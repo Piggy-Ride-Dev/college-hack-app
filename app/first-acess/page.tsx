9231d77e379e80c4fe8be52c4767dff8249c432a
Kaled Barreto <kaledbarreto@gmail.com>
2024-02-21 14:51:47 -0300

    feat: add all components

--- app/first-acess/page.tsx
+++ app/first-acess/page.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 // import logo from "../src/logo.svg";
 import "./styles.scss";
-import { Breadcrumb, Layout, Menu, theme, Image, Select  } from 'antd';
+import { Breadcrumb, Layout, Menu, theme, Image, Select, Button  } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
@@ -17,52 +17,42 @@ const App: React.FC = () => {
   } = theme.useToken();
 
   return (
-    <Layout>
+    <Layout className='layout-container'>
       <Header className='header'>
         {/* <Image src={logo}/> */}
       </Header>
-      <Content style={{ padding: '0 48px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className='content'
+      <Content>
+        <div className='container'
           style={{
-            background: colorBgContainer,
+            // background: colorBgContainer,
             borderRadius: borderRadiusLG,
           }}
         >
-          <h1>Lets Get Started</h1>
-          <p>First, tell me your Institution and Program</p>
-          <div className='input'>
-            <span>Institution</span>
-            <Select
-            defaultValue="lucy"
-            style={{ width: 120 }}
-            // onChange={handleChange}
-            options={[
-                { value: 'jack', label: 'Jack' },
-                { value: 'lucy', label: 'Lucy' },
-                { value: 'Yiminghe', label: 'yiminghe' },
-                { value: 'disabled', label: 'Disabled', disabled: true },
-            ]}
-            />
+        	<div className='content'>
+						<h1>Let's Get Started!</h1>
+						<p>First, tell me your Institution and Program</p>
+						<div className='input'>
+							<span>Institution</span>
+								<Select
+								options={[
+									{ value: '1', label: '1' },
+									{ value: '2', label: '2' },
+									{ value: '3', label: '3' },
+								]}
+								/>
+						</div>
+						<div className='input'>
+							<span>Program</span>
+							<Select
+								options={[
+									{ value: '1', label: '1' },
+									{ value: '2', label: '2' },
+									{ value: '3', label: '3' },
+								]}
+							/>
+						</div>
           </div>
-          <div className='input'>
-            <span>Institution</span>
-            <Select
-            defaultValue="lucy"
-            style={{ width: 120 }}
-            // onChange={handleChange}
-            options={[
-                { value: 'jack', label: 'Jack' },
-                { value: 'lucy', label: 'Lucy' },
-                { value: 'Yiminghe', label: 'yiminghe' },
-                { value: 'disabled', label: 'Disabled', disabled: true },
-            ]}
-            />
-          </div>
+          <Button type="primary" block>Next</Button>
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>
