257c7493e850e91a263a61850132c0aed9b1d072
Kaled Barreto <kaledbarreto@gmail.com>
2024-03-04 13:58:58 -0300

    chore: removing distDir

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-    distDir: "build",
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
