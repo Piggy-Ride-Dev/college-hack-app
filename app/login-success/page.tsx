bb886561e589363c0272c6453725e250d4428cfd
Kaled Barreto <kaledbarreto@gmail.com>
2024-03-04 11:56:21 -0300

    fix: add suspense on login-sucess

--- app/login-success/page.tsx
+++ app/login-success/page.tsx
@@ -1,24 +1,33 @@
 'use client';
 import { redirect, useSearchParams } from "next/navigation";
 import { useEffect } from "react";
+import { Suspense } from 'react';
 
 export default function loginSuccess() {
-  const searchParams = useSearchParams();
-  const jwt = searchParams.get('jwt');
-  const isFirstAccess = searchParams.get('isFirstAccess');
-
-  useEffect(() => {
-    const handleLoginSuccess = () => {
-      jwt && localStorage.setItem('token', jwt);
-      isFirstAccess && redirect('/first-access');
-    };
-
-    handleLoginSuccess();
-  }, [jwt, isFirstAccess]);
+  function LoginSuccessContent() {
+    const searchParams = useSearchParams();
+    const jwt = searchParams.get('jwt');
+    const isFirstAccess = searchParams.get('isFirstAccess');
+  
+    useEffect(() => {
+      const handleLoginSuccess = () => {
+        jwt && localStorage.setItem('token', jwt);
+        isFirstAccess && redirect('/first-access');
+      };
+  
+      handleLoginSuccess();
+    }, [jwt, isFirstAccess]);
+  
+    return (
+      <div>
+        <h1>Login success!</h1>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>Login success!</h1>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginSuccessContent />
+    </Suspense>
   );
 }
