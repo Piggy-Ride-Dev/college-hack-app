0beeb1cffb0eb9118244f6cb316d80dbb75d3c3d
Kaled Barreto <kaledbarreto@gmail.com>
2024-03-07 14:38:13 -0300

    chore: add env

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,18 @@
-import { getApiURL } from "@/api/environment";
 import axios from "axios";
 
 let token = null;
 
-if (typeof localStorage !== 'undefined') {
-  token = localStorage.getItem('token');
+if (typeof localStorage !== "undefined") {
+  token = localStorage.getItem("token");
 }
 
+const isDevelopment = process.env.NEXT_PUBLIC_ENV === "development";
+const baseURL = isDevelopment
+  ? process.env.NEXT_PUBLIC_API_DEV
+  : process.env.NEXT_PUBLIC_API_URL;
+
 export const api = axios.create({
-  baseURL: getApiURL(),
+  baseURL: baseURL,
   timeout: 10000,
   headers: {
     Authorization: `Bearer ${token}`,
