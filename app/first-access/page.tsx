2aa6c819c83408db129b2f096bcf0a0a176afebd
Kaled Barreto <kaledbarreto@gmail.com>
2024-03-12 11:46:54 -0300

    chore: update to antd form

--- app/first-access/page.tsx
+++ app/first-access/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { patchUserClient } from "@/api/userClient";
 import { useGetAllInstitutions } from "@/hooks/useInstitution";
-import { Button, Select } from "antd";
+import { Button, Form, Select } from "antd";
 import { useState } from "react";
 import { useMutation } from "react-query";
 
@@ -21,18 +21,10 @@ export default function firstAccess() {
   const [selectCollege, setSelectCollege] = useState<string>("");
   const [selectProgram, setSelectProgram] = useState<string>("");
 
+  const [form] = Form.useForm();
+  const { Item } = Form;
+
   const { data: institutions } = useGetAllInstitutions();
-
-  const handleSelect1Change = (value: string) => {
-    setSelectCollege(value);
-    console.log(value);
-  };
-
-  const handleSelect2Change = (value: string) => {
-    setSelectProgram(value);
-    console.log(value);
-  };
-
   const patchClientMutation = useMutation(
     async (data: any) => patchUserClient(data),
     {
@@ -41,21 +33,21 @@ export default function firstAccess() {
       },
       onError: (error: any) => {
         console.log(
-          `Um erro aconteceu. Por favor, tente novamente. Erro: ${error.response.data.message}`,
+          `Um erro aconteceu. Por favor, tente novamente. Erro: ${error}`,
         );
       },
     },
   );
 
-  const handleNextClick = async () => {
-    const data = {
-      college: selectCollege,
-      program: selectProgram,
+  const onSubmit = (data: any) => {
+    const values = {
+      college: data["college"],
+      program: data["program"],
     };
 
-    console.log(data);
+    console.log(values);
 
-    patchClientMutation.mutate(data);
+    patchClientMutation.mutateAsync(values);
   };
 
   function getUniqueColleges(colleges: any[]): College[] {
@@ -85,38 +77,40 @@ export default function firstAccess() {
 
   return (
     <main>
-      <div className="flex h-full w-full max-w-screen-lg flex-col justify-between self-center p-6">
+      <Form
+        onFinish={onSubmit}
+        form={form}
+        layout="vertical"
+        className="flex h-full w-full max-w-screen-lg flex-col justify-between self-center p-6"
+      >
         <div className="flex flex-col gap-8 px-6">
           <h1 className="text-2xl">Let's Get Started!</h1>
           <p>First, tell me your Institution and Program</p>
-          <div className="flex w-full flex-col gap-2">
-            <span>Institution</span>
-            <Select
-              size="large"
-              options={options}
-              onChange={handleSelect1Change}
-            />
-          </div>
-          <div className="flex w-full flex-col gap-2">
-            <span>Program</span>
-            <Select
-              size="large"
-              options={programOptions}
-              onChange={handleSelect2Change}
-            />
-          </div>
+          <Item
+            name="college"
+            label="Institution"
+            className="flex w-full flex-col gap-2"
+          >
+            <Select size="large" options={options} />
+          </Item>
+          <Item
+            name="program"
+            label="Program"
+            className="flex w-full flex-col gap-2"
+          >
+            <Select size="large" options={programOptions} />
+          </Item>
         </div>
         <Button
-          className="mt-6"
+          className="mt-6 text-[#474747]"
           size="large"
           type="primary"
+          htmlType="submit"
           block
-          // disabled={true}
-          onClick={handleNextClick}
         >
           Next
         </Button>
-      </div>
+      </Form>
     </main>
   );
 }
